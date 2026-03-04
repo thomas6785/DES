1c079ed586f9c82dadaf17c147522d649c75fe28
THOMAS O'DEA <thomas@odea.com>
2026-03-04 16:06:26 +0000

    standardise timer 2 set up to be the same for all three modes

--- measurement_instrument/Source/main.c
+++ measurement_instrument/Source/main.c
@@ -14,6 +14,8 @@ static uint16 amplitude_value; // This is the final amplitude value after polyno
 static uint16 y_min = 0xFFFF; // For amplitude measurement, we also need to keep track of the minimum value in the sampling window
 static uint16 amplitude_t1_interrupt_counter;
 
+static uint8 current_mode;
+
 sfr16 ADCDATA = 0xD9; // TODO why???
 
 void clear_interrupts_and_timers() { // TODO can't we make all these functions 'inline'? Compiler seems not to like the 'inline' keyword
@@ -23,7 +25,6 @@ void clear_interrupts_and_timers() { // TODO can't we make all these functions '
 	// Stop timers that are being used
 	TR0 = 0;   // Stop Timer 0
 	TR1 = 0;   // Stop Timer 1
-	TR2 = 0;   // Stop Timer 2
 
 	// Clear timer interrupt flags
 	TF0 = 0;
@@ -36,54 +37,75 @@ void clear_interrupts_and_timers() { // TODO can't we make all these functions '
 	TH2 = 0; TL2 = 0;
 }
 
+void timer2setup(void) {
+	// In all modes, timer 2 is used to generate an ISR every 125 ms
+	// which is used to update the display value
+	
+	// Configure timer 2 to generate an interrupt
+	T2CON = (0 << TF2_pos)     | // Overflow flag
+	        (0 << EXF2_pos)    | // External flag
+	        (0 << RCLK_pos)    | // receive flag enable
+	        (0 << TCLK_pos)    | // transmit clock enable
+	        (0 << EXEN2_pos)   | // external enable flag
+	        (1 << TR2_pos)     | // start/stop bit
+	        (0 << CNT2_pos)    | // Timer/counter mode
+	        (0 << CAP2_pos);     // Capture/reload mode
+
+	// Set reload value to 1536
+	// This gives an ISR every 64,000 clock cycles
+	// which is then divided by 27 so the display is updated every 10/64 seconds
+	RCAP2L = 0x00;
+	RCAP2H = 0x06;
+}
+
 /*------------------------------------------------
 Interrupt service routine for timer 2 interrupt.
 Called by the hardware when the interrupt occurs.
 ------------------------------------------------*/
 void timer2_isr(void) interrupt 5 { // interrupt vector at 002BH
+	uint16 new_measurement_value;
 	// TODO split each mode of operation into a separate function here (they can be inlined by compiler)
-	// Check what mode we are in:
-	if ((SWITCHES&0x03) == 0x01) { // TODO I dislike this direct reading of the switches in the ISR, maybe have a global variable that is set in the main loop when we read the switches and then check that here instead
-		// Frequency measurement mode. Increment the register, if it is 25, reset it and record the value on the counter
-		frequency_counter++;
-		if (frequency_counter >= 25) { // TODO make this == not >=
-			frequency_counter = 0;
-			// Now record how many pulses T1 has counted in the last 0.1s. Can simply be bit shifted to get the frequency in Hz.
-			// Pass frequency through IIR filter to smooth it out
-			frequency_value = (TH1 << 8) | TL1;
-			update_display_via_iir(frequency_value);
-			// Reset timer 1 for the next measurement
-			TH1 = 0;
-			TL1 = 0;
+	
+	static uint8 timer2_interrupt_count = 0; // tracks how many ISR's have occured. We only respond to every 27th ISR so we are updating every 10/64 seconds
+	// static variable so it will persist across function calls
+
+	timer2_interrupt_count++;
+	if (timer2_interrupt_count == 27) {
+		timer2_interrupt_count = 0;
+
+		if (current_mode == FREQUENCY_MODE) {
+			new_measurement_value = (TH1 << 8) | TL1; // Read the value of timer 1, which is counting the edges on the input signal
+			// TODO scale and shift
+			TH1 = 0; TL1 = 0; // Reset timer 1 for the next measurement
 		}
-	}
-	else if ((SWITCHES&0x03) == 0x02) {
-			//TODO check if this could be smaller
-			if (amplitude_t1_interrupt_counter >= 17){ // TODO use == not >=
-			amplitude_value = y_max - y_min; // Get the amplitude value for the sampling window
+		else if (current_mode == AMPLITUDE_MODE) {
+			new_measurement_value = y_max - y_min; // Get the amplitude value for the sampling window
+			// TODO scale and shift
 
 			// Reset the max and min for the next sampling window
 			y_max = 0x00;
 			y_min = 0xFFFF;
-			amplitude_t1_interrupt_counter = 0;
-			update_display_via_iir(amplitude_value);
-			}
-			else{
-				amplitude_t1_interrupt_counter++;
-			}
-	}
-	TF2 = 0; // Clear the interrupt
-}  // end timer2 interrupt service routine
+		}
+		else if (current_mode == DC_MODE) {
+			new_measurement_value = ADCDATA & 0x0FFF; // TODO scale and shift
+			// TODO check if this is the correct way to read ADCDATA
+			// TODO check that ADCDATA is always valid for reading at any time
+		}
+		update_display_via_iir(new_measurement_value); // update the display with the new measured value
+	} // TODO move handling for each individual mode to a separate function for readability
+
+	TF2 = 0; // Clear the interrupt flag
+}  // end timer2 ISR
 
 void adc_isr(void) interrupt 6 {
 	uint16 sample_value = (ADCDATAH <<8 | ADCDATAL) & 0x0FFF; //TODO is this correct?
-	if ((SWITCHES&0x03) == 0x00) { // TODO I don't like that we are reading the switches directly in the ISR
+	/*if ((SWITCHES&0x03) == 0x00) { // TODO I don't like that we are reading the switches directly in the ISR
 		// DC measurement mode. Take the sample and update the IIR filter output
 		// IIR Filter
 		sample_value = sample_value & 0x0FFF;
 		update_display_via_iir(sample_value);
 
-	} else if ((SWITCHES&0x03) == 0x02) {
+	} else*/ if ((SWITCHES&0x03) == 0x02) {
 		// Record the highest and lowest value
 		if (sample_value > y_max){
 			y_max = sample_value;
@@ -96,31 +118,21 @@ void adc_isr(void) interrupt 6 {
 }
 
 void setup_dc_measure(void) {
-	// TODO we're using timer 2 here?
-	T2CON = (0 << TF2_pos)     | // Overflow flag
-	        (0 << EXF2_pos)    | // External flag
-	        (0 << RCLK_pos)    | // receive flag enable
-	        (0 << TCLK_pos)    | // transmit clock enable
-	        (0 << EXEN2_pos)   | // external enable flag
-	        (1 << TR2_pos)     | // start/stop bit
-	        (0 << CNT2_pos)    | // Timer/counter mode
-	        (0 << CAP2_pos);     // Capture/reload mode
-
 	// Configure ADC for DC
 	ADCCON1 = (1 << MD1_pos)    | // Operating mode of ADC
 	          (0 << EXT_REF_pos)| // External reference
 	          (0 << CK1_pos)    | // ADC clock divide bits
 	          (0 << CK0_pos)    | // ADC clock divide bits
 	          (3 << AQ_pos)     | // number of ADC clock cycles
-	          (1 << T2C_pos)    | // Which timer to use
-	          (0 << EXC_pos);     // external trigger
+	          (1 << T2C_pos)    | // trigger ADC conversion on timer 2 overflow
+	          (0 << EXC_pos);     // external trigger off
 
-	// Enable ADC interrupt
-	ADCCON2 = (1 << ADCI_pos)   | // ADC interrupt flag
-	          (0 << DMA_pos)    | // ADC DMA flag
-	          (0 << CCONV_pos)  | // ADC conversion complete flag
-	          (0 << SCONV_pos)  | // ADC sequence complete flag
-	          (0 << CS_pos);      // ADC channel select bits
+	// Enable ADC interrupt // TODO this is wrong, we don't need to set the flag ourselves here
+	ADCCON2 = (0 << ADCI_pos)   | // 0: clear ADC interrupt flag
+	          (0 << DMA_pos)    | // 0: not using DMA
+	          (0 << CCONV_pos)  | // 0: disable continuous conversion
+	          (0 << SCONV_pos)  | // 0: no 'single conversion' needed either, we are using timer 2 instead to trigger conversions at regular intervals
+	          (0 << CS_pos);      // select channel 0 (AIN0)
 
 	EA = 1;    // Enable global interrupts
 	EADC = 1;  // Enable ADC interrupt
@@ -133,16 +145,6 @@ void setup_frequency_measure(void) {
 	// every 0.1 s, the value in timer 0 is read and displayed (via the IIR filter),
 	// then timer 0 is reset
 
-	// Configure timer 2 to generate an interrupt every 0.1s (needs a reload value of 10240, and a register counting to 25)
-	T2CON = (0 << TF2_pos)     | // Overflow flag
-	        (0 << EXF2_pos)    | // External flag
-	        (0 << RCLK_pos)    | // receive flag enable
-	        (0 << TCLK_pos)    | // transmit clock enable
-	        (0 << EXEN2_pos)   | // external enable flag
-	        (1 << TR2_pos)     | // start/stop bit
-	        (0 << CNT2_pos)    | // Timer/counter mode
-	        (0 << CAP2_pos);     // Capture/reload mode
-
 	// Enable Timer 2 interrupt
 	//IE = // TODO write entire IE register appropriately and do the same for amplitude measure mode
 	ET2 = 1;
@@ -150,10 +152,6 @@ void setup_frequency_measure(void) {
 	ET0 = 0;
 	EA = 1;    // Enable global interrupts
 	EADC = 0;  // Disable ADC interrupt
-
-	// Set reload value to 10240
-	RCAP2L = 0x00;
-	RCAP2H = 0x28;
 
 	// Configure timer 0 to count the input frequency. Use in mode 1.
 	//TMOD = (0 << GATE_pos) | // Timer 0 gate control
@@ -188,29 +186,16 @@ void setup_amplitude_measure(void) {
 	          (0 << SCONV_pos)  | // ADC sequence complete flag
 	          (0 << CS_pos);      // ADC channel select bits
 
-	// Use Timer 2 to measure sampling window, pulse every 0.1s
-	T2CON = (0 << TF2_pos)     | // Overflow flag
-	        (0 << EXF2_pos)    | // External flag
-	        (0 << RCLK_pos)    | // receive flag enable
-	        (0 << TCLK_pos)    | // transmit clock enable
-	        (0 << EXEN2_pos)   | // external enable flag
-	        (1 << TR2_pos)     | // start/stop bit
-	        (0 << CNT2_pos)    | // Timer/counter mode
-	        (0 << CAP2_pos);     // Capture/reload mode
-	
-	// Set reload value to 0, use a register to count 17 pulses to get a sampling window of 0.1s
-	RCAP2L = 0x00;
-	RCAP2H = 0x00;
-
 	EA = 1;			// Enable global interrupts // TODO use a single write to IE
 	EADC = 1;		// Enable ADC interrupt
 	ET2 = 1;		// Enable Timer 2 interrupt
 }
 
 void main(void) {
-	uint8 prev_switch_value;
+	uint8 prev_mode;
 
 	initialDisplaySetup(); // set up the SPI display, including a display test long enough for humans to see all LEDs light up
+	timer2setup();
 
 	//i = 0;
 	//while(1) {
@@ -221,32 +206,33 @@ void main(void) {
 	// Configure switches for user input and begin reading their values
 	SWITCHES = 0xFF;  // Make switch pins inputs
 	T1 = 1; // put timer 1 counter input in input mode  TODO why is this in main??
-	prev_switch_value = 0x8;
+	prev_mode = 0x8;
 
 	while(1) {
 		// Read switches and enter appropriate mode
-		uint8 switch_value = SWITCHES & 0x03;
-		if (prev_switch_value != switch_value) {
+		current_mode = SWITCHES & 0x03;
+		if (prev_mode != current_mode) {
 			clear_interrupts_and_timers(); // If we are switching mode, disable interrupts and clear timers before switching modes to avoid any issues with leftover state from the previous mode
 			reset_iir(); // Reset the IIR filter for the display when we switch modes (this will set a flag that causes it to overwrite the IIR value on the next update, then return to normal IIR operation)
 
-			if (switch_value == 0x00) { // TODO enumerate these hardcoded values
+			if (current_mode == DC_MODE) {
 				// DC measurement mode
 				spiWrite(MAX7219_DIGIT8_ADDR,				1); // TODO replace these SPI writes with using the LEDs on port 0
 				setup_dc_measure();
-			} else if (switch_value == 0x01) {
+			} else if (current_mode == FREQUENCY_MODE) {
 				// Frequency measurement mode
 				spiWrite(MAX7219_DIGIT8_ADDR,				2);
 				setup_frequency_measure();
-			} else if (switch_value == 0x02) {
+			} else if (current_mode == AMPLITUDE_MODE) {
 				// Amplitude measurement mode
 				spiWrite(MAX7219_DIGIT8_ADDR,				3);
 				setup_amplitude_measure();
-			} else {
-				spiWrite(MAX7219_DIGIT8_ADDR,				0);
-				//setup_dc_measure();
+			} else if (current_mode == DISPLAY_TEST_MODE) {
+				// Display test mode
+				spiWrite(MAX7219_DIGIT8_ADDR,				4);
+				//setup_display_test(); TODO
 			}
 		}
-		prev_switch_value = switch_value;
+		prev_mode = current_mode;
 	}
 }  // end main
