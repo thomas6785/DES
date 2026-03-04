f8404ceb0439d7549845b7ff6efd46edd8ca9170
THOMAS O'DEA <thomas@odea.com>
2026-03-04 16:37:23 +0000

    tidy up interrupts

--- measurement_instrument/Source/main.c
+++ measurement_instrument/Source/main.c
@@ -18,20 +18,29 @@ static uint8 current_mode;
 
 sfr16 ADCDATA = 0xD9; // TODO why???
 
-void clear_interrupts_and_timers() { // TODO can't we make all these functions 'inline'? Compiler seems not to like the 'inline' keyword
-	// Disable all interrupts
-	IE = 0; // IE = Interrupt enable. Different bits map to different interrupts so '0 is no interrupts
+void setup_interrupts_and_timers() { // TODO can't we make all these functions 'inline'? Compiler seems not to like the 'inline' keyword
+	// Configure the interrupts we are using
+	IE =	(1 << EA_pos ) | // global interrupt enable bit
+				(0 << EADC_pos ) |
+				(1 << ET2_pos ) | // keep interrupts from timer 2, they are used to update the display in all modes
+				(0 << ES_pos ) |
+				(0 << ET1_pos ) |
+				(0 << EX1_pos ) |
+				(1 << ET0_pos ) | // keep interrupts from timer 0, they are used for the heartbeat LED in all modes
+				(0 << EX0_pos );
 
-	// Stop timers that are being used
-	TR1 = 0;   // Stop Timer 1
-
-	// Clear timer interrupt flags
-	TF1 = 0;
-	TF2 = 0;
+	// Configure T2 to have priority
+	IP =	(0 << EA_pos ) | // bit is not used for the interrupt priority register
+				(0 << EADC_pos ) |
+				(1 << ET2_pos ) | // T2 has higher priority than T1 - we don't want the blinking LED to interfere with our measurements
+				(0 << ES_pos ) |
+				(0 << ET1_pos ) |
+				(0 << EX1_pos ) |
+				(0 << ET0_pos ) |
+				(0 << EX0_pos );
 
 	// Clear counters to remove leftovers when switching modes
 	TH0 = 0; TL0 = 0;
-	TH1 = 0; TL1 = 0;
 	TH2 = 0; TL2 = 0;
 }
 
@@ -116,17 +125,16 @@ void adc_isr(void) interrupt 6 {
 }
 
 void timer0_isr(void) interrupt 1 {
-	static uint16 heartbeat_counter; // Used to divide ISR frequency so LED blinks slowly. Static so it persists across function calls
+	static uint16 heartbeat_counter = 0; // Used to divide ISR frequency so LED blinks slowly. Static so it persists across function calls
 
 	// This is used for the heartbeat LED blinking. Just toggle the LED and clear the interrupt flag
-	if (heartbeat_counter >= 20) {
-		HEARTBEAT_LED = ~HEARTBEAT_LED; // Toggle LED
+	heartbeat_counter++;
+
+	if (heartbeat_counter == 50) {
+		HEARTBEAT_LED = ~HEARTBEAT_LED & ((SWITCHES & 00100000b) >> 5); // Toggle LED
 		heartbeat_counter = 0;
 	}
-	else{
-		heartbeat_counter++;
-	}
-	TF0 = 0;
+	TF0 = 0; // clear interrupt flag
 }
 
 void setup_dc_measure(void) {
@@ -146,8 +154,7 @@ void setup_dc_measure(void) {
 	          (0 << SCONV_pos)  | // 0: no 'single conversion' needed either, we are using timer 2 instead to trigger conversions at regular intervals
 	          (0 << CS_pos);      // select channel 0 (AIN0)
 
-	EA = 1;    // Enable global interrupts
-	EADC = 1;  // Enable ADC interrupt
+	EADC = 1;  // Enable ADC interrupt for DC measure mode
 }
 
 void setup_frequency_measure(void) {
@@ -157,24 +164,17 @@ void setup_frequency_measure(void) {
 	// every 0.1 s, the value in timer 0 is read and displayed (via the IIR filter),
 	// then timer 0 is reset
 
-	// Enable Timer 2 interrupt
-	//IE = // TODO write entire IE register appropriately and do the same for amplitude measure mode
-	ET2 = 1;
-	ET1 = 0;
-	EA = 1;    // Enable global interrupts
-	EADC = 0;  // Disable ADC interrupt
-
 	// Configure timer 0 to count the input frequency. Use in mode 1.
 	//TMOD = (0 << GATE_pos) | // Timer 0 gate control
 	//       (1 << CT_pos)   | // Timer/counter mode
 	//       (0 << M1_pos)   | // Mode bit 1
 	//       (1 << M0_pos);    // Mode bit 0
-	TMOD = 0x50; //0b00000101; // look at diagram on page 70 for mode 1. Gate is LOW so that we are always counting, and timer is connected to P3.5
+	TMOD = 0x51; //0b00000101; // look at diagram on page 70 for mode 1. Gate is LOW so that we are always counting, and timer is connected to P3.5 TODO use defined bit positions here
 
 	TCON =	(0 << TF1_pos) | // Timer 1 overflow flag off
 					(1 << TR1_pos) | // Timer 1 should be running
 					(0 << TF0_pos) | // Timer 0 overflow flag
-					(0 << TR0_pos) | // Timer 0 run control
+					(1 << TR0_pos) | // Timer 0 run control
 					(0 << IE1_pos) | // External interrupt 1 flag
 					(0 << IT1_pos) | // External interrupt 1 type
 					(0 << IE0_pos) | // External interrupt 0 flag
@@ -196,30 +196,26 @@ void setup_amplitude_measure(void) {
 	          (1 << CCONV_pos)  | // ADC conversion complete flag
 	          (0 << SCONV_pos)  | // ADC sequence complete flag
 	          (0 << CS_pos);      // ADC channel select bits
-
-	EA = 1;			// Enable global interrupts // TODO use a single write to IE
-	EADC = 1;		// Enable ADC interrupt
-	ET2 = 1;		// Enable Timer 2 interrupt
 }
 
-void heartbeat(void){
+void setup_heartbeat(void){
 	// Setup timer 0 to be used for heartbeat LED blinking
-	TMOD = (0 << GATE_pos) | // Timer 0 gate control
-	       (0 << CT_pos)   | // Timer/counter mode
-	       (1 << M1_pos)   | // Mode bit 1 
-	       (0 << M0_pos);    // Mode bit 0
+	//TMOD = (0 << GATE_pos) | // Timer 0 gate control
+	//       (0 << CT_pos)   | // Timer/counter mode
+	//       (1 << M1_pos)   | // Mode bit 1 
+	//       (0 << M0_pos);    // Mode bit 0
+	// TODO
+	TMOD = 0x51;
+	// TODO use defined bit positions here and resolve conflict with configuration for T1 in frequency measure mode
 
 	TCON =	(0 << TF1_pos) | // Timer 1 overflow flag off
-			(0 << TR1_pos) | // Timer 1 should be running
-			(0 << TF0_pos) | // Timer 0 overflow flag
-			(1 << TR0_pos) | // Timer 0 run control
-			(0 << IE1_pos) | // External interrupt 1 flag
-			(0 << IT1_pos) | // External interrupt 1 type
-			(0 << IE0_pos) | // External interrupt 0 flag
-			(0 << IT0_pos);   // External interrupt 0 type
-	
-	ET0 = 1;
-	EA = 1;
+					(0 << TR1_pos) | // Timer 1 should be running
+					(0 << TF0_pos) | // Timer 0 overflow flag
+					(1 << TR0_pos) | // Timer 0 run control
+					(0 << IE1_pos) | // External interrupt 1 flag
+					(0 << IT1_pos) | // External interrupt 1 type
+					(0 << IE0_pos) | // External interrupt 0 flag
+					(0 << IT0_pos);   // External interrupt 0 type
 }
 
 void main(void) {
@@ -227,6 +223,8 @@ void main(void) {
 
 	initialDisplaySetup(); // set up the SPI display, including a display test long enough for humans to see all LEDs light up
 	timer2setup();
+	setup_heartbeat(); // Start the heartbeat LED blinking to show that the system is alive and running
+	setup_interrupts_and_timers();
 
 	//i = 0;
 	//while(1) {
@@ -243,9 +241,8 @@ void main(void) {
 		// Read switches and enter appropriate mode
 		current_mode = SWITCHES & 0x03;
 		if (prev_mode != current_mode) {
-			clear_interrupts_and_timers(); // If we are switching mode, disable interrupts and clear timers before switching modes to avoid any issues with leftover state from the previous mode
+			setup_interrupts_and_timers(); // Set interrupts and timers back to their default configurations
 			reset_iir(); // Reset the IIR filter for the display when we switch modes (this will set a flag that causes it to overwrite the IIR value on the next update, then return to normal IIR operation)
-			heartbeat(); // Start the heartbeat LED blinking to show that the system is alive and running
 
 			if (current_mode == DC_MODE) {
 				// DC measurement mode
@@ -254,7 +251,7 @@ void main(void) {
 			} else if (current_mode == FREQUENCY_MODE) {
 				// Frequency measurement mode
 				spiWrite(MAX7219_DIGIT8_ADDR,				2);
-				setup_frequency_measure();
+				setup_frequency_measure(); 
 			} else if (current_mode == AMPLITUDE_MODE) {
 				// Amplitude measurement mode
 				spiWrite(MAX7219_DIGIT8_ADDR,				3);
