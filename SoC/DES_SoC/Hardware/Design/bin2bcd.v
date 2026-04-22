e7795debae3abf7c6ea68153f8762dd57cb9686c
Aidan Quigney <aidan.quigney@ucdconnect.ie>
2026-04-22 17:48:19 +0100

    update double dabble

--- SoC/DES_SoC/Hardware/Design/bin2bcd.v
+++ SoC/DES_SoC/Hardware/Design/bin2bcd.v
@@ -1,17 +1,28 @@
 // This is a module to convert binary to binary coded decimal values
+// TODO, this should also handle negative numbers with a flag, by checking the MSB of the input. 
+
 
 module bin2bcd #(
     parameter INPUT_WIDTH = 8,
     parameter OUTPUT_WIDTH = INPUT_WIDTH + (INPUT_WIDTH-4)/3+1
 )(  input       [INPUT_WIDTH -1: 0] bin_in,
-    output reg  [OUTPUT_WIDTH-1: 0] bcd_out);
+    input                           enable,
+    input                           clk, 
+    output reg  [OUTPUT_WIDTH-1: 0] bcd_out,
+    output reg                      negative_flag);
 
 
     integer i, j;
     always @(bin_in) begin
         bcd_out = {OUTPUT_WIDTH{1'b0}};
+        negative_flag = 0;
 
-        bcd_out[INPUT_WIDTH-1:0]= bin_in;
+        if (bin_in[INPUT_WIDTH-1] == 1) begin
+            negative_flag = 1;
+            bcd_out[INPUT_WIDTH-1:0] = -bin_in;
+        end else begin
+            bcd_out[INPUT_WIDTH-1:0] = bin_in;
+        end
 
         for (i=0; i <= INPUT_WIDTH-4; i= i+1)
         begin
